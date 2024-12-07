236b0c7f2e5ce969fc92b7c4a39c0a51e97168b0
Bart Chen <bart@entergrid.com>
2024-12-07 15:05:42 -0700

    Fixed error tolerance

--- TestQR.py
+++ TestQR.py
@@ -26,8 +26,9 @@ def RRQR(A, delta):
     k = 0
     R = A.copy()
     norms = np.linalg.norm(R, axis=0)
+    epsilon = delta * np.sqrt(2) / (n+1)
 
-    while np.max(norms) > delta and k != maxrank - 1:
+    while np.max(norms) > epsilon and k != maxrank - 1:
         maxcol = np.where(norms == np.max(norms))[0][0] + k
         x, y = P.copy()[:,k], R.copy()[:,k]
         P[:,k], R[:,k] = P[:,maxcol], R[:,maxcol] ## Swap columns based on norm of cols in lower block Ck
@@ -38,7 +39,28 @@ def RRQR(A, delta):
             Ck = R[k + 1:, k + 1:]
             norms = np.linalg.norm(Ck, axis=0)
             k += 1
-    return P, Qf, R
+            epsilon = np.sqrt(2) * delta / (n - k + 1)
+    return P, Qf, R, k, epsilon
+
+A = np.random.uniform(low = -10, high = 10, size = (20,20))
+for i in range(10):
+    inds = np.random.choice(range(10), 4, replace = False)
+    x = np.zeros(20)
+    for ind in inds:
+        u = np.random.uniform(size = 20)
+        x += np.random.randint(-5,5)*A[:,ind] + u/10
+    A[:,i+10] = x
+
+
+P, Q, R, k, epsilon = RRQR(A, 0)
+
+P1, Q1, R1, k1, epsilon1 = RRQR(A, 0.5)
+
+np.linalg.norm(A@P1 - Q1@R1)
+
+
+AP = A@P
+AP1 = Q@R
 
 n = 79
 m = 60
